0200ed1b5adfeed5f8a62a39ed1b4091c45805f3
kchu01 <chukathy01@gmail.com>
2021-03-09 14:25:47 -0800

    Include MVP

--- src/components/mood-tracker/MoodTracker.jsx
+++ src/components/mood-tracker/MoodTracker.jsx
@@ -1,55 +1,87 @@
 import { Component } from 'react'
 import MoodPoints from '../mood-points/MoodPoints.jsx'
 import MoodNote from '../mood-note/MoodNote.jsx'
+import placeHolderNotes from './placeHolderNotes.js'
 
 export default class MoodTracker extends Component {
-    // // create state and set the initial state boomer way
-    // constructor(props) {
-    //     // invoke super first
-    //     // get functionality from Component
-    //     super(props)
-    //     this.state = {
-    //         points: 99     
-    //     }
-    // }
-
-    // class field declaration
+    //class state
     state = {
-        points: 9
+        points: 0,
+        noteInput: '',
+        noteData: []
     }
 
-    // event handler section
+    // event handlers
     handleIncreaseMood = () => {
-        this.setState((previousState, props) => {
-            return {
-                points: previousState.points + 1
-            }
+        this.setState((prevState, props) => {
+            return { points: prevState.points + 1 }
         })
     }
 
     handleDecreaseMood = () => {
-        this.setState((previousState, props) => {
-            return {
-                points: previousState.points - 1
-            }
+        this.setState((prevState, props) => {
+            return { points: prevState.points - 1 }
         })
     }
 
+    handleInputChange = (e) => {
+        this.setState({
+            noteInput: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.setState((prevState, props) => {
+            const newNoteData = {
+                note: prevState.noteInput,
+                date: new Date().toLocaleDateString(),
+                points: prevState.points
+            }
+            return {
+                noteData: [...prevState.noteData, newNoteData]
+            }
+        }, () => this.setState({ noteInput: '' }))
+    }
+
     render() {
+        const noteComponents = this.state.noteData.map((placeHolderNote, index) => {
+            return (
+                <MoodNote
+                    key={`note ${index}`}
+                    points={placeHolderNote.points}
+                    date={placeHolderNote.date}
+                    note={placeHolderNote.note}
+                />
+            )
+        })
+
         return (
             <div>
                 <MoodPoints points={this.state.points} />
 
-                <button onClick={this.handleIncreaseMood}>Increase Mood</button>
-                <button onClick={this.handleDecreaseMood} >Decrease Mood</button>
+                <button onClick={this.handleIncreaseMood}>Increase</button>
+                <button onClick={this.handleDecreaseMood}>Decrease</button>
 
-                <h3>My notes: </h3>
+                <h3>Mood Notes:</h3 >
 
-                <MoodNote
-                    points={5}
-                    date={'date'}
-                    note={'test note'}
-                />
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='note-input'>New Note:</label>
+
+                    <input
+                        id='note-input'
+                        type='text'
+                        onChange={this.handleInputChange}
+                        placeholder='How do you feel?'
+                    />
+
+                    <input
+                        type='submit'
+                        value='Save Note'
+                    />
+                </form>
+
+                {noteComponents}
             </div>
         )
     }
